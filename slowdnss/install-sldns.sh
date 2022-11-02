78790d334193ceac6418265d2d7dee5bb528d0fe
rokki91 <117232687+rokki91@users.noreply.github.com>
2022-11-02 10:09:07 +0500

    Update install-sldns.sh

--- slowdnss/install-sldns.sh
+++ slowdnss/install-sldns.sh
@@ -2,7 +2,7 @@
 # Slowdns Instalation by Jrtunnel 19-09-2022
 # ==========================================
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/godtrex99/permission/main/ipmini > /root/tmp
+    curl -sS https://raw.githubusercontent.com/rokki91/99ip/main/ipmini > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -20,7 +20,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/godtrex99/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/rokki91/99ip/main/ipmini | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -37,7 +37,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/godtrex99/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/rokki91/99ip/main/ipmini | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
